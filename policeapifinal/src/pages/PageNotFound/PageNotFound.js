6d2a4067424582ee85e1c5aef8f9e551430766e2
Papgooner <tomperkes@yahoo.co.uk>
2022-10-10 16:24:51 +0100

    css for error page

--- policeapifinal/src/pages/PageNotFound/PageNotFound.js
+++ policeapifinal/src/pages/PageNotFound/PageNotFound.js
@@ -1,8 +1,12 @@
+import "./PageNotFound.css";
+
 function PageNotFound() {
     return (
-        <div>
-        <h1>Error 404</h1>
-        <p>Page not found</p>
+        <div id="ErrorpageDiv">
+            <div id="ErrorpageBox">
+        <h1 id="ErrorTitle">Error 404</h1>
+        <p id="ErrorText">Page not found, close the page and try again</p>
+        </div>
         </div>
     )
 }
